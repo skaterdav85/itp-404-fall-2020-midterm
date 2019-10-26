d5f17f2bdf7a358fa9e864247564694948908288
David Tang <dtang@auditboard.com>
2019-10-25 21:57:41 -0700

    Jump to Page

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,18 +7,31 @@ export default class HomePage extends React.Component {
   constructor() {
     super(...arguments);
     this.state = {
+      page: 1,
       tracks: [],
       loading: true
     };
   }
   async componentDidMount() {
-    const json = await getTracksByPage(1);
+    const json = await getTracksByPage(this.state.page);
     this.setState({
       tracks: json.data,
       totalPages: json.meta.pages,
       loading: false
     });
   }
+  handlePageInputChange = event => {
+    this.setState({ page: event.target.value });
+  };
+  goToPage = async () => {
+    this.setState({ loading: true });
+    const json = await getTracksByPage(this.state.page);
+    this.setState({
+      tracks: json.data,
+      totalPages: json.meta.pages,
+      loading: false
+    });
+  };
   render() {
     return (
       <div>
@@ -27,9 +40,15 @@ export default class HomePage extends React.Component {
           <div className="col-8">Total Pages: {this.state.totalPages}</div>
           <div className="col-4 text-right">
             <div className="input-group">
-              <input className="form-control" value={this.state.page} />
+              <input
+                className="form-control"
+                value={this.state.page}
+                onChange={this.handlePageInputChange}
+              />
               <div className="input-group-prepend">
-                <button type="button">Jump to Page</button>
+                <button type="button" onClick={this.goToPage}>
+                  Jump to Page
+                </button>
               </div>
             </div>
           </div>
