0ccb8fb96bfda9b5f38d5d6e5925f6783f2c7ebb
David Tang <dtang@auditboard.com>
2019-10-25 22:08:04 -0700

    Be sure that the list of tracks updates when changing between playlists.

--- src/PlaylistPage.js
+++ src/PlaylistPage.js
@@ -11,6 +11,17 @@ export default class PlaylistPage extends React.Component {
       loading: true
     };
   }
+  async componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.setState({ loading: true });
+      const json = await fetchPlaylist(this.props.match.params.id);
+      this.setState({
+        playlistName: json.data.attributes.name,
+        tracks: json.included || [],
+        loading: false
+      });
+    }
+  }
   async componentDidMount() {
     const json = await fetchPlaylist(this.props.match.params.id);
     this.setState({
